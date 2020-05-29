c13092bbbf9172fa224749ccccc8352e294b2bec
codejamroad <33666843+codejamroad@users.noreply.github.com>
2020-05-29 23:00:52 +0200

    updated function

--- subsystem_1.py
+++ subsystem_1.py
@@ -1,12 +1,8 @@
-import csv
 import cv2
 import landmark_extraction as pointsCalculator
 import nail_detector as nd
 import hand_detector
 
-def zerolistmaker(n):
-    listofzeros = [0] * n
-    return listofzeros
 
 def start(testdata_path, enable_object_detection=False):
     frames = []
@@ -22,22 +18,10 @@ def start(testdata_path, enable_object_detection=False):
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
             frames.append(frame)
             ret, frame = cap.read()
-   
-        
-    keypoints = pointsCalculator.calculate_points(frames)
-    
-    #return hand_side 
+
+        #return hand_side 
     # if 1 - Dorsal
     # if 0 - Palm
     hand_side = nd.palm_dorsal_identifier(testdata_path)
-
-
-    if(hand_side):
-        points_to_csv =  zerolistmaker(40) + keypoints
-    else:
-        points_to_csv =  keypoints + zerolistmaker(40) 
-
-    with open('landmarks.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(points_to_csv)
-
+        
+    pointsCalculator.calculate_points(frames, hand_side)
