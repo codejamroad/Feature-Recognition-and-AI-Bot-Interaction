c662a91a0999fbdddcaefb14754f8b9a54ffac42
codejamroad <33666843+codejamroad@users.noreply.github.com>
2020-05-30 12:20:42 +0200

    corrected filename

--- subsystem_2.py
+++ subsystem_2.py
@@ -2,5 +2,5 @@ import subsystem_2_cnn_predict as cnn
 
 def start(landmarks_data, output_file_name):
     label = cnn.predict(landmarks_data)
-    with open('dog_breeds_reversed.txt', 'w') as writer:
+    with open('label.txt', 'w') as writer:
         writer.write(label)
